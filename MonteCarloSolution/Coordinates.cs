4a6f877ca116747bb079cdcfdf343760a8609270
Jacob Russell <jacob.russell82@outlook.com>
2018-09-15 21:42:55 -0400

    Pushing to gitHub so I can download as a zip to submit for class

---
+++ MonteCarloSolution/Coordinates.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloSolution
+{
+
+    struct Coordinates
+    {
+        public double xCoord, yCoord;
+
+        public Coordinates(int totalPairs) // creates coordinates for each point
+        {
+            Random rand = new Random();
+
+            this.xCoord = rand.NextDouble();
+            this.yCoord = rand.NextDouble();
+        }
+
+    }
+
+    class ManipulateCoordinates
+    {
+
+        public static double Hypoteneus(double xCoord, double yCoord)
+        {
+            double hypot = 0.0;
+            double firstPart = 0.0;
+
+            firstPart = Math.Pow(xCoord, 2) + Math.Pow(yCoord, 2);
+            hypot = Math.Sqrt(firstPart);
+
+            return hypot;
+        }
+
+        public static double PointInCircle(Coordinates[] coordArray)
+        {
+            double hypot = 0.0;
+            double hypotCount = 0.0;
+
+            for (int i = 0; i < coordArray.Length; i++) // 
+            {
+                hypot = Hypoteneus(coordArray[i].xCoord, coordArray[i].yCoord);
+
+                if (hypot <= 1)
+                {
+                    hypotCount++;
+                }
+            }
+
+            return hypotCount;
+        }
+
+    }
+}
--- MonteCarloSolution/Program.cs
+++ MonteCarloSolution/Program.cs
@@ -10,6 +10,29 @@ namespace MonteCarloSolution
     {
         static void Main(string[] args)
         {
+            Random randNum = new Random();
+            int totalPairs = 1000;
+            Coordinates[] coords = new Coordinates[totalPairs];
+            double hypotCount = 0;
+            double mcPI = 0.0;
+            double absDiff = 0;
+
+            for(int i = 0; i < coords.Length; i++) // insert coordinate pairs into array of coordinates
+            {
+                coords[i] = new Coordinates(totalPairs);
+                Console.WriteLine($"{coords[i].xCoord}, {coords[i].yCoord}"); //Used to check 'randomness'
+                System.Threading.Thread.Sleep(31); // Used because if there isn't a pause between numbers many of the coordinates repeat themselves
+            }
+
+            hypotCount = ManipulateCoordinates.PointInCircle(coords);
+
+            mcPI = ((hypotCount / coords.Length) * 4);
+            absDiff = Math.Abs((mcPI - Math.PI));
+
+            Console.WriteLine($"PI with a value of:    {Math.PI}");
+            Console.WriteLine($"Estimated PI value of: {mcPI}");
+            Console.WriteLine($"Absolute difference of: {absDiff}");
+
         }
     }
 }
